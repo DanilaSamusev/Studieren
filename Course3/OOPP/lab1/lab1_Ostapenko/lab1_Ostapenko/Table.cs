fbd3248c791701a0d5ed0d585bc5e605c5bc7282
Woland A <sasha13ostapenko@gmail.com>
2020-09-04 09:38:09 +0300

    Program for lab1_Ostapenko created.

---
+++ Course3/OOPP/lab1/lab1_Ostapenko/lab1_Ostapenko/Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab1_Ostapenko
+{
+    public class Table
+    {
+        private string _name;
+        private double _square;
+
+        public string Name { get => _name; set => _name = value; }
+        public double Square { get => _square; set => _square = value; }
+
+        public Table()
+        {
+            _name = "DefaultTable";
+            _square = 10000;
+        }
+
+        public Table(string name, double square)
+        {
+            _name = name;
+            _square = square;
+        }
+
+        public double GetPrice()
+        {
+            return Math.Pow(_square, 2) / 3 + 500000;
+        }
+
+        public string GetFields()
+        {
+            return $"Стол {_name} с площадью {_square} см кв.";
+        }
+    }
+}
