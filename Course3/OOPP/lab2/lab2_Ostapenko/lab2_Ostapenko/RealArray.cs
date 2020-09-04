d0eae7ac7c9b67581a7880e667a0ad9de078a9b2
Woland A <sasha13ostapenko@gmail.com>
2020-09-04 09:57:52 +0300

    Program lab2_Ostapenko was created.

---
+++ Course3/OOPP/lab2/lab2_Ostapenko/lab2_Ostapenko/RealArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2_Ostapenko
+{
+    public class RealArray
+    {
+        private double[,] _matrix;
+
+        public double[,] Matrix { get => _matrix; set => _matrix = value; }
+
+        public RealArray()
+        {
+        }
+
+        public RealArray(double[,] matrix)
+        {
+            _matrix = matrix;
+        }
+    }
+}
