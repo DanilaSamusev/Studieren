83273175436f9ef14fa69819643a2600fcfd811b
Woland A <sasha13ostapenko@gmail.com>
2020-09-08 09:59:53 +0300

    Added SPO_lab2.

---
+++ Course3/SPO/lab2/SPO_lab2_2/SPO_lab2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SPO_lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("notepad.exe");
+        }
+    }
+}
