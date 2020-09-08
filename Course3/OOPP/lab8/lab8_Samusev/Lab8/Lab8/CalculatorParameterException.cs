c38692b9580ddbbe97d119cb4bad4fb3f011ef75
Aldamar <danila.samusev@mail.ru>
2020-09-08 11:36:15 +0300

    Completed solution for lab 8

---
+++ Course3/OOPP/lab8/lab8_Samusev/Lab8/Lab8/CalculatorParameterException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8
+{
+    public class CalculatorParameterException : Exception
+    {
+        public CalculatorParameterException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Course3/OOPP/lab8/lab8_Samusev/Lab8/Lab8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var calculator = new Calculator();
+                double sum = calculator.CalculateFunction(10, 2, 5);
+
+                Console.WriteLine(sum);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
