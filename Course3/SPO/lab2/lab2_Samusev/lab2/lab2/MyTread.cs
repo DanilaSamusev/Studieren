3d03f746e034b345bdce729000e9290ad0fe5c37
Aldamar <danila.samusev@mail.ru>
2020-09-10 23:04:31 +0300

    Added solution for lab 2

---
+++ Course3/SPO/lab2/lab2_Samusev/lab2/lab2/MyTread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace lab2
+{
+    public class MyThread
+    {
+        public int count; 
+        public Thread thrd;
+        static bool stop; 
+        static string currentName;
+        
+        public MyThread(string name)
+        { 
+            count = 0;
+            thrd= new Thread(Run); 
+            thrd.Name = name; 
+            currentName = name;
+        }
+        
+        public void Run() 
+        {
+            Console.WriteLine("Поток " + thrd.Name + " стартовал. "); 
+            
+            do {
+                count++;
+                
+                if(currentName != thrd.Name)
+                { 
+                    currentName=thrd.Name;
+                    Console.WriteLine("В потоке " + currentName);
+                }
+                
+            } while(stop == false && count < 1000000);
+            
+            stop = true;
+            
+            Console.WriteLine("Поток " + thrd.Name + " завершен.");
+        }
+    }
+}
---
+++ Course3/SPO/lab2/lab2_Samusev/lab2/lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThread mtl =new MyThread("с высоким приоритетом"); 
+            MyThread mt2 =new MyThread("со среднем приоритетом"); 
+            MyThread mt3 = new MyThread("с низким приоритетом");
+            
+            mtl.thrd.Priority = ThreadPriority.Highest; 
+            mt2.thrd.Priority = ThreadPriority.Normal;
+            mt3.thrd.Priority = ThreadPriority.Lowest;
+            
+            mtl.thrd.Start (); 
+            mt2.thrd.Start ();
+            mt3.thrd.Start (); 
+            
+            mtl. thrd. Join() ;
+            mt2.thrd.Join () ;
+            mt3.thrd.Join () ;
+            
+            Console.WriteLine();
+            Console.WriteLine("Поток " + mtl.thrd.Name +
+                              " досчитал до " + mtl.count);
+            Console.WriteLine("Поток " + mt2.thrd.Name +
+                              " досчитал до " + mt2.count);
+            Console.WriteLine("Поток " + mt3.thrd.Name +
+                              " досчитал до " + mt3.count);
+            Console.ReadKey();
+
+        }
+    }
+}
