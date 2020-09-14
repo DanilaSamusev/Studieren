adc9f870720c051e01bb866ad91618938f7f6d4b
Danila Samuseu <Danila_Samuseu@epam.com>
2020-09-14 13:56:04 +0300

    Added report for lab3_Samusev OOPP

---
+++ Course3/OOPP/lab3/lab3_Samusev/Lab3/Lab3/ObjectChild.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class ObjectChild
+    {
+        private int _a;
+        private int _b;
+        private string _operation;
+
+        public int C
+        {
+            get
+            {
+                switch (_operation)
+                {
+                    case "+":
+                        return _a + _b;                       
+                    case "-":
+                        return _a - _b;
+                    case "*":
+                        return _a * _b;
+                }
+
+                return 0;
+            }
+        }
+
+        public ObjectChild()
+        {
+            _a = 1;
+            _b = 2;
+            _operation = "+";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var cast = obj as ObjectChild;
+            return this.C == cast.C;
+        }
+
+        public override string ToString()
+        {
+            return $"a: {_a}, b: {_b}, operation: {_operation}";
+        }
+    }
+}
