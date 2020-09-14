4e85db2e3b6e59135c45aa88e8038e7af132944f
Woland A <sasha13ostapenko@gmail.com>
2020-09-14 20:52:02 +0300

    Completed SPO_lab2_3_Ostapenko.

---
+++ Course3/SPO/lab2/SPO_lab2_3_Ostapenko/SPO_lab2_3_Ostapenko/MyThread.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace SPO_lab2_3_Ostapenko
+{
+    class MyThread
+    {
+        private int _count;
+        private int _countLimit;
+        private static bool _stop = false;
+        private static string _currentName;
+
+        public Thread thread;
+
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                if (value > 0)
+                {
+                    _count = value;
+                }
+            }
+        }
+
+        public int CountLimit
+        {
+            get => _countLimit;
+            set => _countLimit = value;
+        }
+
+        // Создаем новый поток. Обратите внимание на то, что этот конструктор в действительности не запускает потоки на выполнение.
+        public MyThread(string name, int countLimit)
+        {
+            Count = 0;
+            thread = new Thread(new ThreadStart(this.Run))
+            {
+                Name = name
+            };
+            _currentName = name;
+            CountLimit = countLimit;
+        }
+
+        // Начинаем выполнение нового потока.
+
+        private void Run()
+        {
+            Console.WriteLine("Поток " + thread.Name + " стартовал. ");
+            do
+            {
+                Count++;
+                if (_currentName != thread.Name)
+                {
+                    _currentName = thread.Name;
+                    Console.WriteLine("В потоке " + _currentName);
+                }
+            } while (_stop == false && Count < 1000000);
+            _stop = true;
+            Console.WriteLine("Поток " + thread.Name + " завершен.");
+        }
+    }
+}
---
+++ Course3/SPO/lab2/SPO_lab2_3_Ostapenko/SPO_lab2_3_Ostapenko/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace SPO_lab2_3_Ostapenko
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int countLimit = 1000000;
+            var thread1 = new MyThread("Thread1", countLimit);
+            var thread2 = new MyThread("Thread2", countLimit);
+            var thread3 = new MyThread("Thread3", countLimit);
+            var thread4 = new MyThread("Thread4", countLimit);
+
+            // Устанавливаем приоритеты.
+            thread1.thread.Priority += 1;
+            thread2.thread.Priority += 1;
+            thread3.thread.Priority -= 1;
+            thread4.thread.Priority -= 2;
+
+            // Запускаем потоки на выполнение. 
+            thread1.thread.Start();
+            thread2.thread.Start();
+            thread3.thread.Start();
+            thread4.thread.Start();
+
+            thread1.thread.Join();
+            thread2.thread.Join();
+            thread3.thread.Join();
+            thread4.thread.Join();
+
+            Console.WriteLine();
+            Console.WriteLine("Поток " + thread1.thread.Name + " досчитал до " + thread1.Count);
+            Console.WriteLine("Поток " + thread2.thread.Name + " досчитал до " + thread2.Count);
+            Console.WriteLine("Поток " + thread3.thread.Name + " досчитал до " + thread3.Count);
+            Console.WriteLine("Поток " + thread4.thread.Name + " досчитал до " + thread4.Count);
+        }
+    }
+}
