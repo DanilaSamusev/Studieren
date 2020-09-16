76fc7e254dc43bb29c026a8c8c57e6803a9249e5
Aldamar <danila.samusev@mail.ru>
2020-09-16 09:12:49 +0300

    Added lab4_Samusev SPO

---
+++ Course3/SPO/lab4/Lab4/Lab4.Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+ 
+namespace Writer
+{   
+    class Program
+    {
+        static Semaphore sem = new Semaphore(2, 2, "Sema");
+        
+        static void Main(string[] args)
+        {
+            EventWaitHandle starEvent = EventWaitHandle.OpenExisting("starEvent");
+            EventWaitHandle dotEvent = EventWaitHandle.OpenExisting("dotEvent");
+            EventWaitHandle exitEvent = EventWaitHandle.OpenExisting("exitEvent");
+            
+            while (true)
+            {
+                starEvent.Reset();
+                dotEvent.Reset();
+                exitEvent.Reset();
+ 
+                string input = Console.ReadLine();
+ 
+                if (input != "+" && input != "-")
+                {
+                    exitEvent.Set();
+                    break;
+                }
+                else if (input == "+")
+                {
+                    starEvent.Set();
+                }
+                else if (input == "-")
+                {
+                    dotEvent.Set();
+                }
+            }
+            
+            exitEvent.Reset();
+        }
+    }
+}
---
+++ Course3/SPO/lab4/Lab4/Lab4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Administrator
+{
+    class Program
+    {
+        static Semaphore sem = new Semaphore(2, 2, "Sema");
+        
+        static void Main(string[] args)
+        {
+            
+            EventWaitHandle starEvent = new EventWaitHandle(false, EventResetMode.ManualReset, "starEvent");
+            EventWaitHandle dotEvent = new EventWaitHandle(false, EventResetMode.ManualReset, "dotEvent");
+            EventWaitHandle exitEvent = new EventWaitHandle(false, EventResetMode.ManualReset, "exitEvent");
+ 
+            Console.WriteLine("Введите количество процессов Writer:");
+ 
+            int procNum = int.Parse(Console.ReadLine());
+            
+            List<Process> processes=StartProcesses(procNum);
+
+            int index=-1;
+            int disposesProcesses = 0;
+ 
+            while (disposesProcesses!=processes.Count)
+            {
+                index = EventWaitHandle.WaitAny(new[] { starEvent, dotEvent, exitEvent});
+                sem.WaitOne();
+                
+                if (index == 0)
+                {
+                    Console.Write("+");
+                }
+                else if (index == 1)
+                {
+                    Console.Write("-");
+                }
+                else if (index == 2)
+                {
+                    Console.WriteLine();
+                    disposesProcesses++;
+                }
+
+                sem.Release();
+            }
+ 
+            Console.ReadKey();
+        }
+ 
+        private static List<Process> StartProcesses(int num)
+        {
+            List<Process> processes=new List<Process>();
+ 
+            for (int i = 0; i < num; i++)
+            {
+                processes.Add(Process.Start(@"F:\MyWorks\Учёба\Studieren\Course3\SPO\lab4\Lab4\Lab4.Client\bin\Debug\netcoreapp3.0\Lab4.Client.exe"));
+            }
+ 
+            return processes;
+        }
+    }
+}
