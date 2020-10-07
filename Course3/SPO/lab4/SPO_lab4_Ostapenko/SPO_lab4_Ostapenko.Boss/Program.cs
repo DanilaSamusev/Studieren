547d04b5ad5454567419328f18af2a836cf8b441
Woland A <sasha13ostapenko@gmail.com>
2020-10-07 13:10:14 +0300

    Started SPO_lab4_Ostapenko.

---
+++ Course3/SPO/lab4/SPO_lab4_Ostapenko/SPO_lab4_Ostapenko.Boss/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPO_lab4_Ostapenko.Boss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Course3/SPO/lab4/SPO_lab4_Ostapenko/SPO_lab4_Ostapenko.Employee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPO_lab4_Ostapenko.Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
