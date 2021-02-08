8fea265a4a74e2ebbd36549f6f0c0a855f9f6220
DESKTOP-EKT6GIC\Aldamar <danila.samusev@mail.ru>
2021-02-08 22:52:45 +0300

    no message

---
+++ Course3/Semester6/AiPRP/lab1/Samusev/HTML_Parser/HTML_Parser.UI/HTML_Parser.UI/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HTML_Parser.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async Task<string> FindDocument(string[] words)
+        {
+            var parser = new Parser.Parser();
+            return await parser.Parse(words);
+        }
+
+        public async void OpenDocument()
+        {
+            string[] words = Words.Text.Split(", ").Select(x => x.ToLower()).ToArray();
+            string url = await FindDocument(words);
+
+            System.Diagnostics.Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", url);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenDocument();
+        }        
+    }
+}
---
+++ Course3/Semester6/AiPRP/lab1/Samusev/HTML_Parser/HTML_Parser.UI/Parser/Parser.cs
@@ -0,0 +1,66 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class Parser
+    {
+        private HttpClient client;
+        private string[] urls;
+
+        public Parser()
+        {
+            client = new HttpClient();
+            urls = new[] {
+                @"D:\Studieren\Course3\Semester6\AiPRP\lab1\Samusev\HTML_Parser\HTML_Parser.UI\HTML_Parser.UI\Health.html",
+                @"D:\Studieren\Course3\Semester6\AiPRP\lab1\Samusev\HTML_Parser\HTML_Parser.UI\HTML_Parser.UI\Magic.html",
+                @"D:\Studieren\Course3\Semester6\AiPRP\lab1\Samusev\HTML_Parser\HTML_Parser.UI\HTML_Parser.UI\Sport.html"
+            };
+        }
+
+        public async Task<string> Parse(string[] words)
+        {
+            int[] urlCoincidences = new int[urls.Length];
+
+            for (int i = 0; i < urlCoincidences.Length; i++)
+            {
+                int coincidencesCount = 0;
+                
+                string source = File.ReadAllText(urls[i]);
+
+                var parser = new HtmlParser();
+                var document = parser.ParseDocument(source);
+
+                var list = new List<string>();
+
+                foreach(string word in words)
+                {
+                    var divItems = document.QuerySelectorAll("div").Where(element => element.TextContent.Contains(word.ToLower())).Count();
+                    var aItems = document.QuerySelectorAll("a").Where(element => element.TextContent.Contains(word.ToLower())).Count();
+                    var hItems = document.QuerySelectorAll("h1").Where(element => element.TextContent.Contains(word.ToLower())).Count();
+
+                    coincidencesCount += divItems + aItems + hItems;
+                }
+
+                urlCoincidences[i] = coincidencesCount;
+            }
+
+            int max = urlCoincidences.Max();
+            int urlId = Array.IndexOf(urlCoincidences, max);
+
+            string url = urls[urlId];
+          
+            return url;
+        }
+    }
+}
