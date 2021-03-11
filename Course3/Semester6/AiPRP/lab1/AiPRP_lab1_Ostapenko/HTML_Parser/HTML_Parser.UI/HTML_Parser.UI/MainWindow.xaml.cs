8ac7663a46f7d7e566b3be5ee43589a9874023ee
Woland A <sasha13ostapenko@gmail.com>
2021-03-11 13:28:38 +0300

    Useless commit.

---
+++ Course3/Semester6/AiPRP/lab1/AiPRP_lab1_Ostapenko/HTML_Parser/HTML_Parser.UI/HTML_Parser.UI/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HTML_Parser.UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async Task<string> FindDocument(string[] words)
+        {
+            var parser = new Parser.Parser();
+            return await Task.Run(() => parser.Parse(words));
+        }
+
+        public async void OpenDocument()
+        {
+            string[] words = Words.Text.Split(", ").Select(x => x.ToLower()).ToArray();
+            string url = await FindDocument(words);
+
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", url);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenDocument();
+        }
+    }
+}
---
+++ Course3/Semester6/AiPRP/lab1/AiPRP_lab1_Ostapenko/HTML_Parser/HTML_Parser.UI/Parser/Parser.cs
@@ -0,0 +1,58 @@
+﻿using AngleSharp.Html.Parser;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Parser
+{
+	public class Parser
+    {
+		private readonly string[] urls;
+
+        public Parser()
+        {
+            urls = new[] {
+                @"D:\Универ\Studieren\Course3\Semester6\AiPRP\lab1\AiPRP_lab1_Ostapenko\HTML_Parser\HTML_Parser.UI\HTML_Parser.UI\Caps.html",
+                @"D:\Универ\Studieren\Course3\Semester6\AiPRP\lab1\AiPRP_lab1_Ostapenko\HTML_Parser\HTML_Parser.UI\HTML_Parser.UI\Pants.html",
+                @"D:\Универ\Studieren\Course3\Semester6\AiPRP\lab1\AiPRP_lab1_Ostapenko\HTML_Parser\HTML_Parser.UI\HTML_Parser.UI\Shoes.html"
+            };
+        }
+
+        public string Parse(string[] words)
+        {
+            int[] urlCoincidences = new int[urls.Length];
+
+            for (int i = 0; i < urlCoincidences.Length; i++)
+            {
+                int coincidencesCount = 0;
+                
+                string source = File.ReadAllText(urls[i]);
+
+                var parser = new HtmlParser();
+                var document = parser.ParseDocument(source);
+
+                var list = new List<string>();
+
+                foreach(string word in words)
+                {
+                    var divItems = document.QuerySelectorAll("div").Where(element => element.TextContent.Contains(word.ToLower())).Count();
+                    var aItems = document.QuerySelectorAll("p").Where(element => element.TextContent.Contains(word.ToLower())).Count();
+                    var hItems = document.QuerySelectorAll("h1").Where(element => element.TextContent.Contains(word.ToLower())).Count();
+
+                    coincidencesCount += divItems + aItems + hItems;
+                }
+
+                urlCoincidences[i] = coincidencesCount;
+            }
+
+            int max = urlCoincidences.Max();
+            int urlId = Array.IndexOf(urlCoincidences, max);
+
+            string url = urls[urlId];
+
+            return url;
+        }
+    }
+}
