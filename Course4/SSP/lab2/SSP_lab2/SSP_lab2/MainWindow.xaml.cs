03d9853c51e1cab377353e8834cf2a9b772e4faf
Aliaksandr Astapenk1 <aliaksandr_astapenka1@epam.com>
2021-09-03 23:18:38 +0300

    SSP: Completed labs 1-2.

---
+++ Course4/SSP/lab2/SSP_lab2/SSP_lab2/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace SSP_lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var lab2_2 = new lab2_2();
+            lab2_2.Show();
+        }
+    }
+}
